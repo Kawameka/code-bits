4f93b1cc9a11d4733f524a001c305ec11d1cfa4d
Kawameka <tanner.tannerlevine@gmail.com>
2017-10-05 20:13:04 -0700

    changed style of writing code bits to be uniform

--- RubyCodeBits.rb
+++ RubyCodeBits.rb
@@ -1,19 +1,25 @@
-Devise Gem with Bootstrap, Flash Messages
+# Code Bit #1 Start
+# Devise Gem with Bootstrap, Flash Messages
 
-# <% if notice.present? %>
-# 	<p class="alert alert-info"><%= notice %></p>
-# <% end %>
-# <% if alert.present? %>
-#   <p class="alert alert-danger"><%= alert %></p>  
-# <% end %>
+<% if notice.present? %>
+	<p class="alert alert-info"><%= notice %></p>
+<% end %>
+<% if alert.present? %>
+  <p class="alert alert-danger"><%= alert %></p>  
+<% end %>
 
-Checking if a new @whatever is valid
+# Code Bit #1 End
 
-# def create
-#   @whatever = Whatever.create(whatever_params)
-#   if @whatever.valid?
-#     redirect_to root_path
-#   else
-#     render :new, status: :unprocessable_entity
-#   end
-# end
+# Code Bit #2 Start
+# Checking if a new @whatever is valid
+
+def create
+  @whatever = Whatever.create(whatever_params)
+  if @whatever.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+# Code Bit #2 End
